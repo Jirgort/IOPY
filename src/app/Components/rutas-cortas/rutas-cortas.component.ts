fc71a5a8d624c5e5d48ce8ada0fc9862e9642d3e
Dasperless <dariovm13@gmail.com>
2022-10-02 00:47:31 -0600

    [ADD] algoritmo de floyd incompleto

--- src/app/Components/rutas-cortas/rutas-cortas.component.ts
+++ src/app/Components/rutas-cortas/rutas-cortas.component.ts
@@ -10,6 +10,70 @@ export class RutasCortasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var a = 
+    {
+      nodos:[
+        {
+          nombre: "1", 
+          caminos:[
+            {nombre: "3", costo:3},
+            {nombre: "4", costo:1},
+          ]},
+        {
+          nombre: "2", 
+          caminos:[
+            {nombre: "1", costo:4},
+            {nombre: "3", costo:1},
+          ]},
+        {
+          nombre: "3", 
+          caminos:[
+            {nombre: "4", costo:5},
+          ]},
+        {
+          nombre: "4", 
+          caminos:[
+            {nombre: "2", costo:6},
+          ]},
+      ]
+    }
+    this.floyd(a);
+  }
+
+  floyd(nodos:any){
+    var n = nodos.nodos.length;
+    let matriz = this.toMatrix(nodos.nodos);
+    for (let k = 1; k < n; k++) {
+      for (let i = 1; i < n; i++) {
+        for (let j = 1; j < n; j++) {
+          //Minimo
+        }
+      }   
+    }
+  }
+
+  toMatrix(A:any){
+    let matriz = Array(A.length).fill(A.length);
+    let nodesIndex:any = {}
+
+    // Ceros en la diagonal
+    for (let i = 0; i < A.length; i++) {
+      matriz[i][i] = 0;
+    }
+
+    // Objeto con los índices
+    for (let i = 0; i < A.length; i++) {
+      nodesIndex[A.nombre] = i;
+    }
+
+    for (let i = 0; i < A.length; i++) {
+      for (let j = 0; j < A.caminos.length; j++) {
+        let nodeIndex = nodesIndex[A.caminos[j].nombre]
+        matriz[i][nodeIndex]
+      }      
+    }
+    return matriz;
+
   }
 
 }
